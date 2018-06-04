1fb50fa75fdbedd30c9d777b580789647278b1c7
Marcos Cardinot <mcardinot@gmail.com>
2018-06-04 17:07:44 +0100

    publications page: open one box at a time (bibtex and abstract)

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,16 @@
+function toggleBox(parent, boxClass) {
+    isOpen = parent.find(boxClass+'.open').length;
+    $(".abstract.hidden.open").toggleClass('open'); // close all abstracts
+    $(".bibtex.hidden.open").toggleClass('open'); // close all bibtex
+    if (!isOpen) { parent.find(boxClass).toggleClass('open'); }
+};
+
 $(document).ready(function() {
     $(".delete-li").contents().unwrap();
     $('a.abstract').click(function() {
-        $(this).parent().parent().parent().find(".abstract.hidden").toggleClass('open');
+        toggleBox($(this).parent().parent().parent(), ".abstract.hidden");
     });
     $('a.bibtex').click(function() {
-        $(this).parent().parent().parent().find("span.bibtex.hidden").toggleClass('open');
+        toggleBox($(this).parent().parent().parent(), "span.bibtex.hidden");
     });
 });
